fdf7f11828e2c6d60c5b4f44d0791abd1442b971
Lee Cheneler <lacheneler@gmail.com>
2020-05-14 17:28:05 +0100

    New package command (#21)

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   docs: {
     "Getting Started": ["installation", "setting-up-a-package"],
-    Commands: ["build", "lint", "start", "test", "typecheck"],
+    Commands: ["build", "lint", "new-package", "start", "test", "typecheck"],
     Miscellaneous: [
       "command-quick-reference",
       "ide-support",
